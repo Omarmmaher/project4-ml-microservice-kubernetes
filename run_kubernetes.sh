366fefce2fbfa7106f51140a8a5634dc2d06ab1a
Omarmmaher <92268664+Omarmmaher@users.noreply.github.com>
2021-11-21 23:13:38 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=omarmaher2909/udacity
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app --image=$dockerpath --port=8080
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/app 8080:80
